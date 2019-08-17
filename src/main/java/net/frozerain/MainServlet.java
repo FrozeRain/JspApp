0af478059f51792890b83d80d8a42d0305aed6e9
FrozeRain <gazer1999@gmail.com>
2019-08-18 01:35:15 +0300

    commit

---
+++ src/main/java/net/frozerain/MainServlet.java
@@ -0,0 +1,17 @@
+package net.frozerain;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet("/MainServlet")
+public class MainServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
